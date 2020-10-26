c3a94149304805e0e642bf968c9a577e58f3ce14
Jeferson <jeferson.hsa@gmail.com>
2020-10-26 02:08:55 -0300

    adéqua testes de execute.go para novas mudanças

--- parser/execute_test.go
+++ parser/execute_test.go
@@ -1,9 +1,9 @@
 package parser
 
 import (
-	"io"
 	"os"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -15,64 +15,43 @@ func testFile(t *testing.T) *os.File {
 	return f
 }
 
-func TestLoadChat(t *testing.T) {
-	file := testFile(t)
-
-	tests := []struct {
-		name            string
-		reader          io.Reader
-		wantMessagesLen int
-	}{
-		{
-			name:            "",
-			reader:          file,
-			wantMessagesLen: 1054,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := LoadChat(tt.reader); len(got.messages) != tt.wantMessagesLen {
-				t.Errorf("LoadChat() = %v, want %v", len(got.messages), tt.wantMessagesLen)
-			}
-		})
-	}
-}
-
 func TestChat_Parse(t *testing.T) {
 	type fields struct {
-		messages []string
+		messages string
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		want   [][]int64
+		name                 string
+		fields               fields
+		want                 [][]int64
+		wantNumberOfMessages int
 	}{
 		{
-			name:   "ignores empty lines and returns empty map",
-			fields: fields{messages: []string{"", "", "", ""}},
-			want:   [][]int64{},
+			name:                 "ignores empty lines and returns empty map",
+			fields:               fields{messages: "\n\n\n\n"},
+			want:                 [][]int64{},
+			wantNumberOfMessages: 0,
 		},
 		{
 			name: "only counts lines starting with date",
 			fields: fields{
-				messages: []string{
-					"10/9/19, 14:00 - Jeferson: Lorem Lorem Lorem",
-					"Jeferson: Lorem Lorem Lorem\nMais Lorem aqui\nE mais aqui",
-					"Jeferson: E mais uma linha",
-					"11/11/19, 07:00 - Jeferson: Agora em outro dia",
-					"11/11/19, 07:01 - Jeferson: Mas duplicado",
-				},
+				messages: "10/9/19, 14:00 - Jeferson: Lorem Lorem Lorem\n" +
+					"Jeferson: Lorem Lorem Lorem\nMais Lorem aqui\nE mais aqui\n" +
+					"Jeferson: E mais uma linha\n" +
+					"11/11/19, 07:00 - Jeferson: Agora em outro dia\n" +
+					"11/11/19, 07:01 - Jeferson: Mas duplicado\n",
 			},
-			want: [][]int64{{1570579200, 1}, {1573430400, 2}},
+			want:                 [][]int64{{1570579200, 1}, {1573430400, 2}},
+			wantNumberOfMessages: 3,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			chat := &Chat{
-				messages: tt.fields.messages,
+			got, nMes := Parse(strings.NewReader(tt.fields.messages))
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse() = %v, want %v", got, tt.want)
 			}
-			if got := chat.Parse(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("parse() = %v, want %v", got, tt.want)
+			if nMes != tt.wantNumberOfMessages {
+				t.Errorf("Parse(), nMessages = %v, want %v", nMes, tt.wantNumberOfMessages)
 			}
 		})
 	}
@@ -93,32 +72,13 @@ func TestConvert(t *testing.T) {
 	}
 }
 
-func TestIntegration(t *testing.T) {
-	file := testFile(t)
-	got := LoadChat(file).Parse()
-	if len(got) != 500 {
-		t.Errorf("LoadChat().Parse() = %v, want 500", len(got))
-	}
-}
-
-func BenchmarkLoad_Chat(b *testing.B) {
-	file, err := os.OpenFile("./tests/small_conversation.txt", os.O_RDONLY, 0444)
-	if err != nil {
-		b.Errorf("file for tests couldn't be found: %v", err)
-	}
-	for n := 0; n < b.N; n++ {
-		LoadChat(file)
-	}
-}
-
 func BenchmarkChat_Parse(b *testing.B) {
-	chat := Chat{messages: []string{
-		"10/9/19, 07:00 - Jeferson: Lorem Lorem Lorem",
-		"Jeferson: Lorem Lorem Lorem\nMais Lorem aqui\nE mais aqui",
-		"Jeferson: E mais uma linha",
-		"11/11/19, 07:00 - Jeferson: Agora em outro dia",
-	}}
+	input := "10/9/19, 07:00 - Jeferson: Lorem Lorem Lorem\n" +
+		"Jeferson: Lorem Lorem Lorem\nMais Lorem aqui\nE mais aqui\n" +
+		"Jeferson: E mais uma linha\n" +
+		"11/11/19, 07:00 - Jeferson: Agora em outro dia\n"
+	reader := strings.NewReader(input)
 	for n := 0; n < b.N; n++ {
-		chat.Parse()
+		Parse(reader)
 	}
 }
